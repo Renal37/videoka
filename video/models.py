182bd5b0d2e0becd33b908e0cb2ffc3bb00fc377
Renal17 <renalplaycsgo@gmail.com>
2024-09-17 08:46:59 +0300

    add video

---
+++ video/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Video(models.Model):
+    title = models.CharField(max_length=150)
+    desctiption = models.TextField()
+    thumbnail = models.ImageField(upload_to="videos/thumbnail")
+    source = models.FileField(upload_to='videos')
