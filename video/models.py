6e26bdf3d41a036dfe3735be46440eb9f2661b20
Renal17 <renaplaycsgo@gmail.com>
2024-09-17 10:01:06 +0300

    add video and migrate files app superuser

--- video/models.py
+++ video/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Video(models.Model):
     title = models.CharField(max_length=150)
-    desctiption = models.TextField()
+    description = models.TextField()
     thumbnail = models.ImageField(upload_to="videos/thumbnail")
     source = models.FileField(upload_to='videos')
